9c2b35513a182dd4cda4db5f452d3d3160c7618d
Larry Price <larry.price.dev@gmail.com>
2017-04-05 16:25:21 -0400

    before threads

--- dbustest/random_data.py
+++ dbustest/random_data.py
@@ -1,5 +1,6 @@
 import dbus.service
 import random
+import time
 
 class RandomData(dbus.service.Object):
     def __init__(self, bus_name):
@@ -10,3 +11,14 @@ class RandomData(dbus.service.Object):
                          in_signature='i', out_signature='s')
     def quick(self, bits=8):
         return str(random.getrandbits(bits))
+
+    @dbus.service.method("com.larry_price.test.RandomData",
+                         in_signature='i', out_signature='s')
+    def slow(self, bits=8):
+        maximum = 2**bits
+        num = random.randint(0, min(512, maximum)-1)
+        while num.bit_length() < bits:
+            num += random.randint(0, min(512, maximum-int(num))-1)
+            time.sleep(1)
+
+        return str(num)
