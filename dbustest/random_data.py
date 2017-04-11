ac504f7043dfb3719572c2c14b45ab1d74f37346
Larry Price <larry.price.dev@gmail.com>
2017-04-11 15:23:07 -0400

    signals (part 3)

--- dbustest/random_data.py
+++ dbustest/random_data.py
@@ -4,34 +4,32 @@ import time
 
 import threading
 class SlowThread(object):
-    def __init__(self, bits):
-        self.finished = threading.Event()
+    def __init__(self, bits, callback):
+        self._callback = callback
         self.result = ''
 
         self.thread = threading.Thread(target=self.work, args=(bits,))
         self.thread.start()
         self.thread_id = str(self.thread.ident)
 
-    @property
-    def done(self):
-        return self.finished.is_set()
-
     def work(self, bits):
-        num = str(random.randint(0, 1))
-        while bits > 1:
+        num = ''
+
+        while True:
             num += str(random.randint(0, 1))
             bits -= 1
             time.sleep(1)
 
-        self.result = str(int(num, 2))
-        self.finished.set()
+            if bits <= 0:
+                break
+
+        self._callback(self.thread_id, str(int(num, 2)))
 
 class RandomData(dbus.service.Object):
     def __init__(self, bus_name):
         super().__init__(bus_name, "/com/larry_price/test/RandomData")
 
         random.seed()
-        self.threads = []
 
     @dbus.service.method("com.larry_price.test.RandomData",
                          in_signature='i', out_signature='s')
@@ -41,21 +39,9 @@ class RandomData(dbus.service.Object):
     @dbus.service.method("com.larry_price.test.RandomData",
                          in_signature='i', out_signature='s')
     def slow(self, bits=8):
-        thread = SlowThread(bits)
-        self.threads.append(thread)
+        thread = SlowThread(bits, self.slow_result)
         return thread.thread_id
 
-    @dbus.service.method("com.larry_price.test.RandomData",
-                         in_signature='s', out_signature='s')
-    def slow_result(self, thread_id):
-        thread = [t for t in self.threads if t.thread_id == thread_id]
-        if not thread:
-            return 'No thread matching id %s' % thread_id
-
-        thread = thread[-1]
-        if thread.done:
-            result = thread.result
-            self.threads.remove(thread)
-            return result
-
-        return ''
+    @dbus.service.signal("com.larry_price.test.RandomData", signature='ss')
+    def slow_result(self, thread_id, result):
+        pass
